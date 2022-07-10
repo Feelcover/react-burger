beb9426a06c9e75f9daee7a6592a342e90be4a09
Andrey Chebanov <pioneerbeat@yandex.ru>
2022-07-10 16:15:32 +0300

    refactoring api and add loadingState

--- src/utils/Api.jsx
+++ src/utils/Api.jsx
@@ -1,14 +1,23 @@
 export const Api = {
-    url: 'https://norma.nomoreparties.space/api/ingredients',
-    headers: {
-        'Content-Type': 'aplication.json'
-    }
+  url: "https://norma.nomoreparties.space/api/ingredients",
+  headers: {
+    "Content-Type": "application.json",
+  },
 };
 
-
 export const processResponse = (res) => {
-    if (res.ok) {
-        return res.json();
-    }
-    return Promise.reject(new Error(`Ошибка: Код ${res.status}`));
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject(new Error(`Ошибка: Код ${res.status}`));
+};
+
+export function getData() {
+  return fetch(`${Api.url}`)
+    .then(processResponse)
+    .catch((err) => {
+      error = err;
+    });
 }
+
+export let error = null;
